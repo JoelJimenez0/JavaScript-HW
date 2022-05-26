5e9c14a8756ee739b1b593e2dc8840330f06f296
JoelJimenez0 <joelklk10@gmail.com>
2022-05-26 01:17:08 -0400

    added the input and changed de js and css structures

--- calculator/js/Cal.js
+++ calculator/js/Cal.js
@@ -1,26 +1,18 @@
-let display = document.getElementById('display');
-let buttons = Array.from(document.getElementsByClassName('button'));
-
-buttons.map( button => {
-    button.addEventListener('click', (e) =>{
-        switch(e.target.innerText){    
-            case 'C':
-                display.innerText = '';
-                break;
-            case '←':
-                if(display.innerText){
-                    display.innerText = display.innerText.slice(0, -1);
-                }
-                break;
-            case '=':
-                try{
-                    display.innerText = eval(display.innerText);
-                }catch{
-                    display.innerText = 'SyntaxError!';
-                }
-                break;
-            default:
-                display.innerText += e.target.innerText;
-        }
-});
-} );
+let result= document.getElementById('display');
+let cal=(Number)=>{
+    result.value+=Number;
+}
+let Result=()=>{
+    try{
+        result.value=eval(result.value)
+    }
+    catch(err){
+        alert("SyntaxError");
+    }
+}
+function clr(){
+    result.value= "";
+}
+function del(){
+    result.value= result.value.slice(0,-1); 
+}
